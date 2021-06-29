00fb5dbde24962879da0cfefa610a7d5ba5237ff
João Pedro Shimidt Vasconcelos <jp.shimidt@gmail.com>
2021-06-28 21:53:37 -0300

    Add project files.

---
+++ ExemploFiltro/ExemploFiltro/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExemploFiltro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Exemplo para aprendizagem de filtro em listas...
+
+            int contador = 0;
+            String[] listaAleatoria = {"Ricardo", "Jeferson", "Luiza", "Luana", "Vitoria", "José", "Luiz", "Pepe", "Larissa", "Rick"};
+
+            for (int i=0; i < listaAleatoria.Length; i++)
+            {
+                Console.WriteLine(i+1 + ". " + listaAleatoria[i]);
+            }
+
+            Console.WriteLine("Por qual nome você está procurando? ");
+            String elementoFiltrado = Console.ReadLine();
+
+            for (int j=0; j < listaAleatoria.Length; j++)
+            {
+
+                if (elementoFiltrado.ToUpper() == listaAleatoria[j].ToUpper())
+                {
+                    String[] listaFiltrada = {listaAleatoria[j]};
+                    int posicao = j+1;
+                    contador = contador + 1;
+
+                    Console.WriteLine("Esse nome possui na lista! Ele está na posição " + posicao +".");
+                } else {
+                }
+            }
+
+            if (contador == 0)
+            {
+                Console.WriteLine("Esse nome não possui na lista! Tente novamente.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Exercicio01/Exercicio01/Exercicio01.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio01
+{
+    class Exercicio01
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Olá, me diga seu nome: ");
+            string nome = Console.ReadLine();
+
+            string nomeInvertido = new string(nome.Reverse().ToArray());
+
+            Console.Write(nomeInvertido);
+            Console.ReadLine();
+        }
+
+        static public String InverterNome(string nome)
+        {
+            char[] ArrayChar = nome.ToCharArray();
+            Array.Reverse(ArrayChar);
+            return new string(ArrayChar);
+        }
+    }
+}
