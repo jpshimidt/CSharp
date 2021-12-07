7f3889730961a707204e2cddc73d1a843a9064de
João Pedro Shimidt Vasconcelos <joao.vasconcelos@xpi.com.br>
2021-12-07 14:07:29 -0300

    06 ByteBank

---
+++ ByteBankAlura/06-ByteBank/ContaCorrente.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _06_ByteBank
+{
+    public class ContaCorrente
+    {
+        public Cliente titular;
+        public int agencia;
+        public int numero;
+        private double saldo;
+
+        public bool Sacar(double valor)
+        {
+            if (saldo < valor)
+            {
+                return false;
+            }
+
+            saldo -= valor;
+            return true;
+        }
+
+        public void Depositar(double valor)
+        {
+            this.saldo += valor;
+        }
+
+
+        public bool Transferir(double valor, ContaCorrente contaDestino)
+        {
+            if (saldo < valor)
+            {
+                return false;
+            }
+
+            saldo -= valor;
+            contaDestino.Depositar(valor);
+            return true;
+        }
+
+        public double ObterSaldo()
+        {
+            return saldo;
+        }
+
+        public void DefinirSaldo(double saldo)
+        {
+            if (saldo < 0)
+            {
+                return;
+            }
+
+            this.saldo = saldo;
+        }
+    }
+}
---
+++ ByteBankAlura/06-ByteBank/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _06_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente();
+
+
+            Console.WriteLine(conta.ObterSaldo());
+            Console.ReadKey();
+        }
+    }
+}
