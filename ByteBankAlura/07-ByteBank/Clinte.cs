bd1522487d46c5c9f42f730cee463e0c44872217
João Pedro Shimidt Vasconcelos <jp.shimidt@gmail.com>
2021-12-08 00:46:25 -0300

    07-ByteBank / Constructor

---
+++ ByteBankAlura/07-ByteBank/Clinte.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _07_ByteBank
+{
+    public class Cliente
+    {
+        public string nome;
+        public string Profissao { get; set; }
+        private string _cpf;
+
+        public string Cpf
+        {
+            get
+            {
+                return _cpf;
+            }
+            set
+            {
+                if (value.Length > 11)
+                {
+                    Console.WriteLine("Cpf inválido");
+                }
+                _cpf = value;
+            }
+        }
+    }
+}
