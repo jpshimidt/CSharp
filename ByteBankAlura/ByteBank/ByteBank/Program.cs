1d34fd3bb2411070c24430f096ad967779404755
João Pedro Shimidt Vasconcelos <jp.shimidt@gmail.com>
2021-12-09 00:26:21 -0300

    ByteBank - modulo III

---
+++ ByteBankAlura/ByteBank/ByteBank/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using ByteBank.Funcionarios;
+
+namespace ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario carlos = new Funcionario(1);
+
+            carlos.Nome = "Carlos";
+            carlos.Salario = 2000;
+            carlos.CPF = "464.571.078.85";
+
+            Console.WriteLine(carlos.Nome);
+            Console.WriteLine(carlos.GetBonificacao());
+            
+            Console.ReadLine();
+        }
+    }
+}
