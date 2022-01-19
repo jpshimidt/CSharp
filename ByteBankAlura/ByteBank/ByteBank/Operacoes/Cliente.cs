d67ba6ff7726a46db3875ef3f0f2919ad2264f1a
João Pedro Shimidt Vasconcelos <joao.vasconcelos@xpi.com.br>
2022-01-19 13:24:54 -0300

    Classes Cliente e ContaCorrente

---
+++ ByteBankAlura/ByteBank/ByteBank/Operacoes/Cliente.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ByteBank.Operacoes
+{
+    public class Cliente
+    {
+        public string nome;
+        public string Profissao { get; set; }
+        private string _cpf;
+
+        public string Cpf
+        {
+            get
+            {
+                return _cpf;
+            }
+            set
+            {
+                if (value.Length > 11)
+                {
+                    Console.WriteLine("Cpf inválido");
+                }
+                _cpf = value;
+            }
+        }
+    }
+}
